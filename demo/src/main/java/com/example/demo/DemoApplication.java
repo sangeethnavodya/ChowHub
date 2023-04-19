5f41af60a920a6c59ca654c249b255955c58a0ec
Sangeeth <Navodya>
2023-04-19 11:21:36 +0530

    Hya

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication { ff
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
