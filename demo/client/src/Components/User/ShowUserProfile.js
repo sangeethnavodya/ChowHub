261e70fa16eebaccf6aff4a702ee569191462ef7
sangeethnavodya <pererakasn.19@uom.lk>
2023-04-25 12:39:41 -0700

    create follow

--- demo/client/src/Components/User/ShowUserProfile.js
+++ demo/client/src/Components/User/ShowUserProfile.js
@@ -10,6 +10,10 @@ function Profile() {
     const [details, setdetails] = useState([])
     const [isLoading, setIsLoading] = useState(true);
     const [isUpload, setIsUpload] = useState(false)
+    const [FollowBTN, setFollowBTN] = useState('Follow');
+    const [isFollow, setIsFollow] = useState(false)
+    const [needToInit, setneedToInit] = useState(false)
+
     const [image, setImage] = useState({
         profileURL: "",
     })
@@ -29,27 +33,91 @@ function Profile() {
         }
         fetchData()
     }, []);
+
+    useEffect(() => {
+        setIsLoading(true)
+        async function fetchData() {
+            await axios.get('http://localhost:8080/follow/' + localStorage.getItem('userId') + "/" + localStorage.getItem('otherId')).then(
+                user => {
+                    console.log(user)
+                    if (Object.keys(user.data).length === 0) {
+                        console.log("d")
+                        setIsLoading(false)
+                        setFollowBTN("Follow")
+                        setneedToInit(true)
+                        setIsFollow(false)
+                    }
+                    else if(!user.data[0].isFollowed){
+                        setFollowBTN("Follow")
+                        setneedToInit(false)
+                        setIsFollow(false)
+                        
+                    }
+                    else if(user.data[0].isFollowed){
+                        setFollowBTN("Unfollow")
+                        setneedToInit(false)
+                        setIsFollow(true)
+                    }
+                    else{
+                        setIsLoading(false)
+                        setFollowBTN("Unfollow")
+                        setneedToInit(false)
+                        setIsFollow(true)
+                    }
+                    
+                }
+            );
+        }
+        fetchData();
+    }, []);
+    const [Follow, setFollow] = useState({
+        isFollowed: true,
+        followerId: localStorage.getItem('userId'),
+        userid: localStorage.getItem('otherId')
+
+    })
+
     if (isLoading) {
         return <div>Loading post details...</div>;
     }
+    function handleFollow() {
+        if (needToInit){
+            axios.post("http://localhost:8080/follow/upload",Follow)
+            setneedToInit(false)
+            setFollowBTN("Unfollow")
+        }
+        else if(!needToInit&&isFollow){
+            axios.put("http://localhost:8080/follow/"+ localStorage.getItem('userId') + "/" + localStorage.getItem('otherId'))
+            setFollowBTN("Follow")
+        }
+        else if(!needToInit&&!isFollow){
+            axios.put("http://localhost:8080/follow/unfollow/"+ localStorage.getItem('userId') + "/" + localStorage.getItem('otherId'))
+            setFollowBTN("UnFollow")
+        }
+    }
+
+
+
+
 
 
 
     return (
 
         <div>
-  
-                <Card className='profile-pic'>
-                    <img
-                        src={details.data.profileURL}
-                        height='100px'
-                        width='100px'
-                        alt='Profile'
-                    />
-                </Card>
-         
+
+            <Card className='profile-pic'>
+                <img
+                    src={details.data.profileURL}
+                    height='100px'
+                    width='100px'
+                    alt='Profile'
+                />
+            </Card>
+
             <Card className='name-label'>
                 <h1>{details.data.name}'s Profile</h1>
+                <Button onClick={() => handleFollow()}>{FollowBTN}</Button>
             </Card>
             <Card className='name-label'>
                 <ShowOtherUser />
