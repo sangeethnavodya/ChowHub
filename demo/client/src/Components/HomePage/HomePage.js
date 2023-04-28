22973ff76d4a4cfc144c2dea3b0340465b6b477a
DulsaraNethmin <nethmin@orangehrm.us.com>
2023-04-28 22:34:51 +0530

    add navbar

--- demo/client/src/Components/HomePage/HomePage.js
+++ demo/client/src/Components/HomePage/HomePage.js
@@ -1,35 +1,50 @@
 import React from "react";
-import {Breadcrumb, Button, Row, Space} from "antd";
+import { Button, Row, Table, Space, Layout, Menu } from "antd";
 import { useNavigate } from "react-router-dom";
 import SeePost from "../Post/ShowPost";
 
+const { Header, Content } = Layout;
+
 function HomePage() {
-    const navigate = useNavigate();
-    function handleProfile() {
-     navigate('/yourProf')
-    }
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <Row>
-                <div>
-                    <Button type="primary" onClick={() => {
-                        localStorage.removeItem('userId');
-                        localStorage.removeItem('userEmail');
-                        localStorage.removeItem('name');
-                        window.location.href = '/';
-                    }}
+  function handleProfile() {
+    navigate("/yourProf");
+  }
 
-                    >Logout</Button>
-                </div>
-                <Space direction='horizontal' className='Nav-bar'>
-                    <a href="/yourProf" className='anchor'>See your Profile</a>
-                    <a href="/createPosts" className='anchor'>Create Post</a>
-                    <a href="/showAllUsers" className='anchor'>Show All Users</a>
-                </Space>
-            </Row>
-            <SeePost />
-        </>
-    )
+  function handleLogout() {
+    localStorage.removeItem("userId");
+    localStorage.removeItem("userEmail");
+    localStorage.removeItem("name");
+    navigate("/");
+  }
+
+  return (
+    <>
+      <Layout>
+        <Header>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1">Home</Menu.Item>
+            <Menu.Item key="2" onClick={() => navigate("/createPosts")}>
+              Create Post
+            </Menu.Item>
+            <Menu.Item key="3" onClick={() => navigate("/showAllUsers")}>
+              Show All Users
+            </Menu.Item>
+            <Menu.Item key="4" onClick={() => handleProfile()}>
+              Your Profile
+            </Menu.Item>
+            <Menu.Item key="5" onClick={() => handleLogout()}>
+              Logout
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content>
+          <SeePost />
+        </Content>
+      </Layout>
+    </>
+  );
 }
-export default HomePage
+
+export default HomePage;
