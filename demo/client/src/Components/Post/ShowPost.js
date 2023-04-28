2d57e7f504ddce5f8c2a95fb098325f41c5d5eee
DulsaraNethmin <nethmin@orangehrm.us.com>
2023-04-28 22:55:04 +0530

    change post layout

--- demo/client/src/Components/Post/ShowPost.js
+++ demo/client/src/Components/Post/ShowPost.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Card } from 'antd';
-import Meta from 'antd/es/card/Meta';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card } from "antd";
+import Meta from "antd/es/card/Meta";
 
-import '../Post/post.css';
-import ReactComponent from './ReactComponent';
-import CommentComponent from '../Comment/CommentComponent';
-import ShowComments from '../Comment/ShowComment';
+import "../Post/post.css";
+import ReactComponent from "./ReactComponent";
+import CommentComponent from "../Comment/CommentComponent";
+import ShowComments from "../Comment/ShowComment";
 
 function PostList() {
   const [postList, setPostList] = useState([]);
@@ -16,14 +16,13 @@ function PostList() {
   useEffect(() => {
     async function fetchData() {
       setIsLoading(true);
-      const response = await axios.get('http://localhost:8080/post/get');
+      const response = await axios.get("http://localhost:8080/post/get");
       setPostList(response.data);
       setIsLoading(false);
-      console.log(response)
+      console.log(response);
     }
     fetchData();
   }, []);
-
 
   if (isLoading) {
     return <div>Loading post details...</div>;
@@ -31,29 +30,20 @@ function PostList() {
 
   return (
     <>
-
       {postList.map((post, index) => (
-
-        <Card
-          className="post-card"
-          key={post.id}
-          style={{ width: 300 }}
-          cover={
-            <>
-              {post.image1 && <img alt="example" src={post.image1} />}
-              {post.image2 && <img alt="example" src={post.image2} />}
-              {post.image3 && <img alt="example" src={post.image3} />}
-              {post.image4 && <img alt="example" src={post.image4} />}
-              {post.image5 && <img alt="example" src={post.image5} />}
-            </>
-          }
-        >
+        <Card className="post-card" key={post.id} style={{ width: 300 }}>
+          <div className="image-grid">
+            {post.image1 && <img alt="example" src={post.image1} />}
+            {post.image2 && <img alt="example" src={post.image2} />}
+            {post.image3 && <img alt="example" src={post.image3} />}
+            {post.image4 && <img alt="example" src={post.image4} />}
+            {post.image5 && <img alt="example" src={post.image5} />}
+          </div>
           <Meta title={post.userName} />
           <Meta title={post.caption} />
           <ReactComponent data={post} />
           <ShowComments data={post} />
           <CommentComponent data={post} />
-
         </Card>
       ))}
     </>
